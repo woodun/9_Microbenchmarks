58ca681e408161147d395eb47127cc4602f0b59c
woodun <hwang07@email.wm.edu>
2018-03-01 15:34:13 -0800

    debug

--- cache_hit_and_prefetch_validation_2/UnifiedMemoryStreams.cu
+++ cache_hit_and_prefetch_validation_2/UnifiedMemoryStreams.cu
@@ -108,7 +108,7 @@ int main(int argc, char **argv)
 		
 	///////////////////////////////////////////////////////////////////CPU data begin	
 	int iterations = 100;
-	////////size(int) = 4, 32 = 128b, 256 = 1kb, 32 * 32 = 1024 = 4kb, 262144 = 1mb, 524288 = 2mb.
+	////////size(int) = 4, 32 = 128b, 256 = 1kb, 32 * 32 = 1024 = 4kb, 262144 = 1mb, 16384 * 32 = 512 * 1024 = 524288 = 2mb.
 	int data_stride = 524288;/////2mb. Pointing to the next page.
 	//int data_size = 524288000;/////1000 * 2mb. ##### size = iteration * stride. ##### This can support 1000 iteration. The 1001st iteration starts from head again.
 	int data_size = iterations * data_stride;/////size = iteration * stride = 100 2mb pages.
