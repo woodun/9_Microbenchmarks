f0c4f0a9bac98420c027f9eb3ab7e5bbff239d3f
woodun <hwang07@email.wm.edu>
2018-03-02 16:26:07 -0800

    debug

--- cache_flush_test/UnifiedMemoryStreams.cu
+++ cache_flush_test/UnifiedMemoryStreams.cu
@@ -58,6 +58,7 @@ __global__ void tlb_latency_test(int *A, int iterations, int *B, float clock_rat
 	long long int end_time = 0;///////////clock	
 	start_time = clock64();///////////clock
 		
+	P_chasing(0, A, 16, B, 16 * 524288, clock_rate);/////warmup GPU
 	P_chasing(0, A, 16, B, 16 * 32, clock_rate);/////warmup TLB
 	P_chasing(0, A, 16, B, 0, clock_rate);/////warmup cache
 	P_chasing(0, A, 16, B, 0, clock_rate);/////try to generate cache hits	
@@ -97,7 +98,9 @@ __global__ void tlb_latency_test_3(int *A, int iterations, int *B, float clock_r
 	start_time = clock64();///////////clock
 		
 	//P_chasing(0, A, 1, B, 31 * 32, clock_rate);/////warmup TLB
+	P_chasing(6, A, 16, B, 17 * 524288, clock_rate);/////warmup GPU
 	P_chasing(7, A, 16, B, 524288, clock_rate);/////try to generate TLB hit
+	P_chasing(8, A, 16, B, 0, clock_rate);/////try to generate cache hits
 	P_chasing(9, A, 16, B, 524288 + 16 * 32, clock_rate);/////try to generate cache miss	
 	
 	end_time=clock64();///////////clock
