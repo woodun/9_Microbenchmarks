e8d8b2f33ebd983c9441a2f2caad6cf8a6a74901
woodun <hwang07@email.wm.edu>
2018-03-05 17:22:54 -0800

    debug

--- cache_line_size_validation_L2_only/cache_line_size_validation_L2_only.cu
+++ cache_line_size_validation_L2_only/cache_line_size_validation_L2_only.cu
@@ -57,7 +57,10 @@ __global__ void tlb_latency_test(int *A, int iterations, int *B, float clock_rat
 	P_chasing(0, A, 16, B, 0 * 32, clock_rate);/////TLB warmup
 	P_chasing(1, A, 16, B, 0 * 32 + 1, clock_rate);/////make them in the same page, and hit near in cache lines
 	P_chasing(4, A, 16, B, 0 * 32 + 4, clock_rate);/////make them in the same page, and hit near in cache lines	
-	P_chasing(8, A, 16, B, 0 * 32 + 8, clock_rate);/////////////make them in the same page, and hit far in cache lines	
+	P_chasing(7, A, 16, B, 0 * 32 + 7, clock_rate);/////////////make them in the same page, and hit far in cache lines
+	P_chasing(8, A, 16, B, 0 * 32 + 8, clock_rate);/////////////make them in the same page, and hit far in cache lines
+	P_chasing(9, A, 16, B, 0 * 32 + 9, clock_rate);/////////////make them in the same page, and hit far in cache lines
+	P_chasing(15, A, 16, B, 0 * 32 + 15, clock_rate);////////////make them in the same page, and hit far in cache lines
 	P_chasing(16, A, 16, B, 0 * 32 + 16, clock_rate);////////////make them in the same page, and hit far in cache lines
 	P_chasing(24, A, 16, B, 0 * 32 + 24, clock_rate);////////////make them in the same page, and hit far in cache lines
 	P_chasing(1, A, 16, B, 1 * 32, clock_rate);/////make them in the same page, and miss near in cache lines
