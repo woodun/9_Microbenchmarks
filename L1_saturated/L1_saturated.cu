916d7b31737d46f1c9a5253e592fbd8cc9c0eee7
woodun <hwang07@email.wm.edu>
2018-03-14 16:31:21 -0700

    debug

--- L1_saturated/L1_saturated.cu
+++ L1_saturated/L1_saturated.cu
@@ -65,9 +65,9 @@ __device__ void P_chasing_2(int mark, int *A, int iterations, int *B, int starti
 			j = A[j];
 		}	
 
-		B[0] = j;		
+				
 	}	
-	
+	B[0] = j;
 	end_time=clock64();//////clock
 	long long int total_time = end_time - start_time;//////clock
 	printf("inside%d:%fms\n", mark, (total_time / (float)clock_rate) / ((float)iterations * 10));//////clock, average latency
