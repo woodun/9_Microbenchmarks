2d50681fd9bb8f7f5789dee2fb0036ed7a71b228
woodun <hwang07@email.wm.edu>
2018-03-21 16:00:31 -0700

    debug

--- measurement_overhead_test_per_request/measurement_overhead_test_per_request.cu
+++ measurement_overhead_test_per_request/measurement_overhead_test_per_request.cu
@@ -51,11 +51,13 @@ __device__ void P_chasing(int mark, int *A, int iterations, int *B, int *C, long
 	}
 	*/
 	
+		asm(".reg .u32 t1;\n\t"
+		".reg .u64 t2;\n\t");
+	
 	for (int it = 0; it < iterations; it++){
 		
-	asm(".reg .u32 t1;\n\t"
-		".reg .u64 t2;\n\t"
-		"mul.wide.s32 	t1, %3, 4;\n\t"	
+
+		asm("mul.wide.s32 	t1, %3, 4;\n\t"	
 		"add.s64 	t2, t1, %4;\n\t"		
 		"mov.u64 	%0, %clock64;\n\t"		
 		"ld.global.u32 	%2, [t1];\n\t"
