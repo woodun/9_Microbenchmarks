113a6058f62d05d0baf15e1a172c9d476cfa5565
woodun <hwang07@email.wm.edu>
2018-03-21 18:10:10 -0700

    debug

--- measurement_overhead_test_per_request/measurement_overhead_test_per_request.cu
+++ measurement_overhead_test_per_request/measurement_overhead_test_per_request.cu
@@ -42,8 +42,8 @@ __device__ void P_chasing1(int mark, int *A, int iterations, int *B, int *C, lon
 	}
 	
 	end_time=clock64();//////clock
-	long long int total_time = end_time - start_time;//////clock
-	printf("inside%d:%fms\n", mark, (total_time / (float)clock_rate) / ((float)iterations));//////clock, average latency
+	//long long int total_time = end_time - start_time;//////clock
+	//printf("inside%d:%fms\n", mark, (total_time / (float)clock_rate) / ((float)iterations));//////clock, average latency
 	
 	B[0] = j;
 }
