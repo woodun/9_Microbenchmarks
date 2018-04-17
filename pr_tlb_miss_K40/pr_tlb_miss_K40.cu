367bd13427044342be276e5cd8a4e387ff97e664
woodun <hwang07@email.wm.edu>
2018-04-17 16:24:44 -0700

    debug

--- pr_tlb_miss_K40/pr_tlb_miss_K40.cu
+++ pr_tlb_miss_K40/pr_tlb_miss_K40.cu
@@ -167,7 +167,7 @@ int main(int argc, char **argv)
 	for(int data_stride = 2 * 256 * 1024; data_stride <= 2 * 256 * 1024; data_stride = data_stride + 1){/////////2mb stride
 		//printf("###################data_stride%d#########################\n", data_stride);
 	//for(int mod = 1024 * 256 * 2; mod > 0; mod = mod - 32 * 1024){/////kepler L2 1.5m
-	for(long long int mod = 2 * 256 * 1024 * 32; mod <= 4294967296; mod = mod * 2){/////kepler L2 1.5m /////kepler L1 16KB ////////saturate the L1 not L2
+	for(long long int mod = 2 * 256 * 1024 * 32; mod <= 2147483648; mod = mod * 2){////2147483648 = 8gb, 4294967296 = 16gb.
 		counter++;
 		///////////////////////////////////////////////////////////////////CPU data begin
 		//int data_size = 2 * 256 * 1024 * 32;/////size = iteration * stride = 32 2mb pages.
