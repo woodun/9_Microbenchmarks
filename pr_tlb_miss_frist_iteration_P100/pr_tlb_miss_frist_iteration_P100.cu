1b0ec2031a3ac3e01d845c62e0259492d5e261ff
woodun <hwang07@email.wm.edu>
2018-04-19 19:30:20 -0700

    debug

--- pr_tlb_miss_frist_iteration_P100/pr_tlb_miss_frist_iteration_P100.cu
+++ pr_tlb_miss_frist_iteration_P100/pr_tlb_miss_frist_iteration_P100.cu
@@ -7,7 +7,7 @@
 #include <helper_cuda.h>
 #include <time.h>
 
-///////////per request timing. L1 enabled. L1 tlb misses commonly occur when data size reach 4gb. L2 tlb misses sparsely appear at data size 8gb. Page table context switches also appear more often at data size 8gb.
+///////////per request timing. L1 enabled. L1 tlb misses commonly occur when data size reach 4gb. L2 tlb misses sparsely appear at data size 8gb. Page table context switches also appear more often at data size 8gb. tlb prefetches? and it cannot be saturated.
 
 //typedef unsigned char byte;
 
