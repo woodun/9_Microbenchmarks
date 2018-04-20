d54d2128ec48fca823522d401b9e815a4e31fbeb
woodun <hwang07@email.wm.edu>
2018-04-20 00:48:47 -0400

    debug

--- pr_tlb_miss_second_iteration_K40/pr_tlb_miss_second_iteration_K40.cu
+++ pr_tlb_miss_second_iteration_K40/pr_tlb_miss_second_iteration_K40.cu
@@ -9,7 +9,7 @@
 
 ///////////per request timing. L1 enabled. 
 ///////////L1 tlb misses commonly occur when data size reach 4gb. L2 tlb misses sparsely appear at data size 8gb. 
-///////////Page table context switches (600s, they are not error) also appear more often at data size 8gb.
+///////////Page table context switches (600s and 900s, they are not error) also appear more often at data size 8gb.
 ///////////page size is 2mb.
 
 //typedef unsigned char byte;
