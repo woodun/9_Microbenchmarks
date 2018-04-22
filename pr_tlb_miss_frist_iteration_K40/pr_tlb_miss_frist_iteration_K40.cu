210416e68b653e89b01ec7c8f5578b5e682ac007
woodun <hwang07@email.wm.edu>
2018-04-22 01:54:48 -0400

    debug

--- pr_tlb_miss_frist_iteration_K40/pr_tlb_miss_frist_iteration_K40.cu
+++ pr_tlb_miss_frist_iteration_K40/pr_tlb_miss_frist_iteration_K40.cu
@@ -125,7 +125,7 @@ __global__ void tlb_latency_test(int *A, long long int iterations, int *B, int *
 	
 	///////////kepler L2 has 48 * 1024 = 49152 cache lines. But we only have 1024 * 4 slots in shared memory.
 	//P_chasing1(0, A, iterations + 0, B, C, D, 0, clock_rate, data_stride);////////saturate the L2
-	P_chasing2(0, A, iterations, B, C, D, 0, clock_rate, data_stride);////////partially print the data
+	P_chasing2(0, A, reduced_iter, B, C, D, 0, clock_rate, data_stride);////////partially print the data
 	
 	 __syncthreads();
 }
@@ -169,10 +169,11 @@ int main(int argc, char **argv)
     pFile = fopen ("output.txt","w");		
 	
 	int counter = 0;
-	for(int data_stride = 2 * 256 * 1024; data_stride <= 2 * 256 * 1024; data_stride = data_stride + 1){/////////2mb stride
+	/////////change the data stride as to observe if the latency increase is caused by iteration(cache) or stride(tlb)
+	for(int data_stride = 2 * 8 * 1024; data_stride <= 2 * 256 * 1024; data_stride = data_stride * 2){/////////64 * 1024 = 256kb, 128 * 1024 = 512kb, 512 * 1024 = 2mb
 		//printf("###################data_stride%d#########################\n", data_stride);
 	//for(int mod = 1024 * 256 * 2; mod > 0; mod = mod - 32 * 1024){/////kepler L2 1.5m
-	for(long long int mod = 2 * 256 * 1024 * 8; mod <= 2147483648; mod = mod * 2){////1073741824 = 4gb, 2147483648 = 8gb, 4294967296 = 16gb.
+	for(long long int mod = 2 * 256 * 1024; mod <= 256 * 256 * 1024; mod = mod * 2){////268435456 = 1gb, 536870912 = 2gb, 1073741824 = 4gb, 2147483648 = 8gb, 4294967296 = 16gb.
 		counter++;
 		///////////////////////////////////////////////////////////////////CPU data begin
 		//int data_size = 2 * 256 * 1024 * 32;/////size = iteration * stride = 32 2mb pages.
@@ -209,7 +210,7 @@ int main(int argc, char **argv)
 		cudaMemcpy(CPU_data_out_time, GPU_data_out_time, sizeof(long long int) * iterations, cudaMemcpyDeviceToHost);
 				
 		fprintf(pFile, "###################data_stride%d#########################\n", data_stride);
-		fprintf (pFile, "###############Mod%d##############%d\n", mod, mod / (2 * 256 * 1024));
+		fprintf (pFile, "###############Mod%lld##############%lld\n", mod, iterations);
 		for (long long int it = 0; it < iterations; it++){			
 			fprintf (pFile, "%d %fms %lldcycles\n", CPU_data_out_index[it], CPU_data_out_time[it] / (float)clock_rate, CPU_data_out_time[it]);
 			//fprintf (pFile, "%d %fms\n", it, CPU_data_out_time[it] / (float)clock_rate);
