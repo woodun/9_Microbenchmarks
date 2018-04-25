45912a65a69a6210a7852d0e471a49a3b3167bd7
woodun <hwang07@email.wm.edu>
2018-04-24 18:14:08 -0700

    debug

--- launch_all.sh
+++ launch_all.sh
@@ -2,7 +2,7 @@
 
 for configs in pr_tlb_miss_first_iteration_P100 pr_tlb_miss_second_iteration_P100
 do
-printf "\r\n currently running: %s\r\n\r\n" $configs
+printf "\r\ncurrently running: %s\r\n\r\n" $configs
 cd $configs
 make
 ./$configs
