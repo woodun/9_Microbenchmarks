6330beaf797e31c693d3208fdc333b5a4caea0e8
woodun <hwang07@email.wm.edu>
2018-04-27 17:40:56 -0700

    DEBUG

--- pr_page_fault_pinned_second_iteration_random_K40/pr_page_fault_pinned_second_iteration_random_K40.cu
+++ pr_page_fault_pinned_second_iteration_random_K40/pr_page_fault_pinned_second_iteration_random_K40.cu
@@ -38,19 +38,19 @@ void init_cpu_data(unsigned *A, unsigned size, unsigned stride, unsigned mod, lo
 	//	A[i]=(i + stride);
    	//}
 	
-	long long int rand_sequence[iterations];
+	long long int rand_sequence[iterations - 1];
 	
 	//////random sequence offset 0
-	for(long long int i = 0; i < iterations; i++){
+	for(long long int i = 0; i < iterations - 1; i++){
 		rand_sequence[i] = i;
 	}
 	//srand (time(NULL));
 	srand (1);
-	shuffle(rand_sequence, iterations);
+	shuffle(rand_sequence, iterations - 1);
 	
 	long long int previous_rand_num;
 	long long int rand_num = rand_sequence[0] * stride;	
-	for(long long int i = 1; i < iterations; i++){		
+	for(long long int i = 1; i < iterations - 1; i++){		
 		previous_rand_num = rand_num;		
 		rand_num = rand_sequence[i] * stride;		
 		A[previous_rand_num]=(unsigned)rand_num;
@@ -64,7 +64,7 @@ void init_cpu_data(unsigned *A, unsigned size, unsigned stride, unsigned mod, lo
 	//shuffle(rand_sequence, iterations);
 	
 	rand_num = rand_sequence[0] * stride + 7;	
-	for(long long int i = 1; i < iterations; i++){		
+	for(long long int i = 1; i < iterations - 1; i++){		
 		previous_rand_num = rand_num;		
 		rand_num = rand_sequence[i] * stride + 7;		
 		A[previous_rand_num]=(unsigned)rand_num;
