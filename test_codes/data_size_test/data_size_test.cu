b868fd47a4e17c6654b6987f5daf0f9e35977a4a
woodun <hwang07@email.wm.edu>
2018-05-08 18:15:29 -0700

    debug

--- test_codes/data_size_test/data_size_test.cu
+++ test_codes/data_size_test/data_size_test.cu
@@ -93,7 +93,7 @@ __device__ void P_chasing2(int mark, long long int *A, long long int iterations,
 		"shl.b64 	t3, %6, 3;\n\t"
 		"add.u64 	t4, t3, %5;\n\t"		
 		"mov.u64 	%0, %clock64;\n\t"		
-		"ld.global.u32 	%2, [t2];\n\t"
+		"ld.global.u64 	%2, [t2];\n\t"
 		"st.shared.u64 	[t4], %2;\n\t"
 		"mov.u64 %1, %clock64;"
 		: "=l"(start_time), "=l"(end_time), "=l"(j) : "l"(j), "l"(A), "l"(s_index), "l"(it));		
