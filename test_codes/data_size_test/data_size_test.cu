72c9296efaf98887ab7ea8476d3742518e9bb358
woodun <hwang07@email.wm.edu>
2018-05-09 13:06:39 -0700

    debug

--- test_codes/data_size_test/data_size_test.cu
+++ test_codes/data_size_test/data_size_test.cu
@@ -109,7 +109,7 @@ __device__ void P_chasing2(int mark, long long int *A, long long int iterations,
 		".reg .u32 t6;\n\t"
 		"cvta.to.shared.u64 	t5, %0;\n\t"
 		"cvt.u32.u64 	t6, t5;\n\t"
-		: "l"(s_index));
+		:: "l"(s_index));
 		
 		for (int it = 0; it < iterations; it++){//////////it here is limited by the size of the shared memory
 			
@@ -117,7 +117,7 @@ __device__ void P_chasing2(int mark, long long int *A, long long int iterations,
 			"add.s64 	t2, t1, %4;\n\t"
 			"shl.b32 	t3, %6, 3;\n\t"
 			"add.s32 	t4, t3, t6;\n\t"		
-			"mov.u64 	%0, %clock64;\n\t"		
+			"mov.u64 	%0, %clock64;\n\t"
 			"ld.global.u64 	%2, [t2];\n\t"
 			"st.shared.u64 	[t4], %2;\n\t"
 			"mov.u64 %1, %clock64;"
