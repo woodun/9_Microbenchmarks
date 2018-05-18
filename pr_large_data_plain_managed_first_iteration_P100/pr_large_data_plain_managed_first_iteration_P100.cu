b5a347d52495b36f1cce22b95b83cc8a4b4cbe80
woodun <hwang07@email.wm.edu>
2018-05-18 11:34:53 -0700

    debug

--- pr_large_data_plain_managed_first_iteration_P100/pr_large_data_plain_managed_first_iteration_P100.cu
+++ pr_large_data_plain_managed_first_iteration_P100/pr_large_data_plain_managed_first_iteration_P100.cu
@@ -103,9 +103,9 @@ __device__ void P_chasing2(int mark, long long int *A, long long int iterations,
 __global__ void tlb_latency_test(long long int *A, long long int iterations, long long int *B, float clock_rate, long long int mod, long long int data_stride){
 			
 	P_chasing2(1, A, iterations/4, B, 0, clock_rate, data_stride);//////////////migrate the first 8gb
-	P_chasing2(1, A, iterations/8, B, 0, clock_rate, data_stride);//////////////access the first 4gb again
-	P_chasing2(1, A, 3 * iterations/8, B, 0, clock_rate, data_stride);///////////migrate another 12gb
-	P_chasing2(1, A, iterations/4, B, 0, clock_rate, data_stride);//////////////which 4gb of the first 8gb is left?
+	P_chasing2(0, A, iterations/8, B, 0, clock_rate, data_stride);//////////////access the first 4gb again
+	P_chasing2(0, A, 3 * iterations/8, B, 0, clock_rate, data_stride);///////////migrate another 12gb
+	P_chasing2(0, A, iterations/4, B, 0, clock_rate, data_stride);//////////////which 4gb of the first 8gb is left?
 	//P_chasing2(1, A, iterations, B, 0, clock_rate, data_stride);
 	//P_chasing2(0, A, iterations, B, mod - data_stride + 3, clock_rate, data_stride);
 	
