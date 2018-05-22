35df2773db5a6007b985e501f7e828642a35c426
woodun <hwang07@email.wm.edu>
2018-05-22 12:41:23 -0700

    debug

--- pr_large_data_plain_managed_first_iteration_P100/pr_large_data_plain_managed_first_iteration_P100.cu
+++ pr_large_data_plain_managed_first_iteration_P100/pr_large_data_plain_managed_first_iteration_P100.cu
@@ -25,7 +25,7 @@ void init_cpu_data(long long int* A, long long int size, long long int stride, l
 		for (long long int i = 16; i < size - stride; i = i + stride){
 			A[i]=(i + stride);
 		}		
-		A[size - stride2 + 16]=0;
+		A[size - stride + 16]=0;
 	}
 	
 	if(0){////////////reversed
@@ -227,8 +227,8 @@ int main(int argc, char **argv)
 	checkCudaErrors(cudaMalloc(&GPU_data_out, sizeof(long long int) * 2));			
 	
 	int counter = 0;	
-	//for(long long int data_stride = 1 * 4 * 1024; data_stride <= 1 * 64 * 1024; data_stride = data_stride * 2){
-	for(long long int data_stride = 1 * 32 * 1024; data_stride <= 1 * 32 * 1024; data_stride = data_stride * 2){
+	for(long long int data_stride = 1 * 4 * 1024; data_stride <= 1 * 64 * 1024; data_stride = data_stride * 2){
+	//for(long long int data_stride = 1 * 32 * 1024; data_stride <= 1 * 32 * 1024; data_stride = data_stride * 2){
 
 	//plain managed
 	printf("*\n*\n*\n plain managed\n");	
