66b9249f08fe83fb332b2aaa87f4bb2a640db03a
woodun <hwang07@email.wm.edu>
2018-05-29 19:20:01 -0700

    debug

--- runtime_managed_vs_memcpy/runtime_managed_vs_memcpy.cu
+++ runtime_managed_vs_memcpy/runtime_managed_vs_memcpy.cu
@@ -157,15 +157,15 @@ int main(int argc, char **argv)
 	
 	//plain managed
 	printf("###################\n#########################managed\n");
-	///32 * 64 <==> 512, 
-	for(long long int data_stride = 1 * 1 * 1; data_stride <= 1 * 64 * 1024; data_stride = data_stride * 2){/////512 is 4m, see what happens after 2m. log2(512 * 1024) = 19. 20 positions.
+	///32 * 64 <==> 1 * 512 * 1024 (8gb), 32 * 512 <==> 1 * 64 * 1024 (8gb), 
+	for(long long int data_stride = 1 * 1 * 1; data_stride <= 1 * 8 * 1024; data_stride = data_stride * 2){/////512 is 4m, see what happens after 2m. log2(512 * 1024) = 19. 20 positions.
 	for(long long int mod = 536870912; mod <= 536870912; mod = mod * 2){////134217728 = 1gb, 268435456 = 2gb, 536870912 = 4gb, 1073741824 = 8gb, 2147483648 = 16gb, 4294967296 = 32gb, 8589934592 = 64gb. (index)
 	for(long long int clock_count = 1; clock_count <= 1024; clock_count = clock_count * 2){/////11 positions.
 		///////////////////////////////////////////////////////////////////CPU data begin		
 		//long long int data_size = mod;
 		long long int data_size = data_stride;
-		data_size = data_size * 32;
-		data_size = data_size * 512;
+		data_size = data_size * 128;
+		data_size = data_size * 1024;
 		//long long int iterations = mod / data_stride;////32 * 32 * 4 / 32 * 2 = 256
 	
 		long long int *CPU_data_in;
@@ -191,7 +191,7 @@ int main(int argc, char **argv)
 		clock_gettime(CLOCK_REALTIME, &ts1);
 
 		////may want to use more thread to see clock_count effect
-		Page_visitor<<<32, 512>>>(CPU_data_in, data_stride, clock_count);///////////////1024 per block max
+		Page_visitor<<<128, 1024>>>(CPU_data_in, data_stride, clock_count);///////////////1024 per block max
 		///////////////////////////////////////////////////32 * 64 * 1 * 512 * 1024 = 8gb.
 		cudaDeviceSynchronize();
 				
@@ -213,14 +213,14 @@ int main(int argc, char **argv)
 	}
 
 	printf("###################\n#########################memcpy + kernel\n");
-	for(long long int data_stride = 1 * 1 * 1; data_stride <= 1 * 64 * 1024; data_stride = data_stride * 2){////////question: when using smaller stride to migrate the whole 2M, is managed still better than memcpy?
+	for(long long int data_stride = 1 * 1 * 1; data_stride <= 1 * 8 * 1024; data_stride = data_stride * 2){////////question: when using smaller stride to migrate the whole 2M, is managed still better than memcpy?
 	for(long long int mod = 536870912; mod <= 536870912; mod = mod * 2){////134217728 = 1gb, 268435456 = 2gb, 536870912 = 4gb, 1073741824 = 8gb, 2147483648 = 16gb, 4294967296 = 32gb, 8589934592 = 64gb. (index)
 	for(long long int clock_count = 1; clock_count <= 1024; clock_count = clock_count * 2){
 		///////////////////////////////////////////////////////////////////CPU data begin		
 		//long long int data_size = mod;
 		long long int data_size = data_stride;
-		data_size = data_size * 32;
-		data_size = data_size * 512;
+		data_size = data_size * 128;
+		data_size = data_size * 1024;
 		//long long int iterations = mod / data_stride;////32 * 32 * 4 / 32 * 2 = 256
 	
 		long long int *CPU_data_in;
@@ -250,7 +250,7 @@ int main(int argc, char **argv)
 		struct timespec ts2;
 		clock_gettime(CLOCK_REALTIME, &ts2);
   
-		Page_visitor<<<32, 512>>>(GPU_data_in, data_stride, clock_count);///////////////1024 per block max
+		Page_visitor<<<128, 1024>>>(GPU_data_in, data_stride, clock_count);///////////////1024 per block max
 		///////////////////////////////////////////////////32 * 512 * 2 = 32gb, 32 * 128 * 2 = 8gb, 32 * 64 * 2 = 4gb, 32 * 32 * 2 = 2gb
 		cudaDeviceSynchronize();
 				
