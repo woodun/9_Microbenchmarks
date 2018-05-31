57ca47c04227d92a338fcebe6f95c238a42479eb
woodun <hwang07@email.wm.edu>
2018-05-30 19:11:36 -0700

    debug

--- runtime_managed_vs_memcpy_8k_stride/runtime_managed_vs_memcpy_8k_stride.cu
+++ runtime_managed_vs_memcpy_8k_stride/runtime_managed_vs_memcpy_8k_stride.cu
@@ -161,7 +161,8 @@ int main(int argc, char **argv)
 	///is it still true that in multi threads the dynamic page threshold is still 64k? no, it seems to be 8k.
 	for(long long int data_stride = 1 * 1 * 1024; data_stride <= 1 * 1 * 1024; data_stride = data_stride * 2){
 	//for(long long int data_stride = 1 * 1 * 1; data_stride <= 1 * 8 * 1024; data_stride = data_stride * 2){/////512 is 4m, see what happens after 2m. log2(512 * 1024) = 19. 20 positions.
-	for(long long int mod = 536870912; mod <= 536870912; mod = mod * 2){////134217728 = 1gb, 268435456 = 2gb, 536870912 = 4gb, 1073741824 = 8gb, 2147483648 = 16gb, 4294967296 = 32gb, 8589934592 = 64gb. (index)
+	for(long long int block_num = 16; block_num <= 2048; block_num = block_num + 16){////////up to 64gb
+////134217728 = 1gb, 268435456 = 2gb, 536870912 = 4gb, 1073741824 = 8gb, 2147483648 = 16gb, 4294967296 = 32gb, 8589934592 = 64gb. (index)
 	//for(long long int clock_count = 128; clock_count <= 8192; clock_count = clock_count * 2){/////11 positions.
 	for(long long int clock_count = 1; clock_count <= 1; clock_count = clock_count * 2){/////11 positions.
 		///////////////////////////////////////////////////////////////////CPU data begin		
