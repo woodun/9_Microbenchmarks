95e7e3ff032cba4a084822befb12c36e5f803887
woodun <hwang07@email.wm.edu>
2018-06-01 12:53:36 -0700

    debug

--- runtime_managed_intensity/runtime_managed_intensity.cu
+++ runtime_managed_intensity/runtime_managed_intensity.cu
@@ -67,11 +67,13 @@ __global__ void Page_visitor(long long int *A, long long int data_stride, long l
 	asm("mov.u32 %0, %smid;" : "=r"(smid) );
 	int warpid = 1;
 	asm("mov.u32 %0, %warpid;" : "=r"(warpid) );
-	int ctaid = 1;			
+	int ctaid = 1;
 	asm("mov.u32 %0, %ctaid.x;" : "=r"(ctaid) );
+	int nctaid = 1;
+	asm("mov.u32 %0, %nctaid;" : "=r"(nctaid) );
 	if(smid == 0){
 		if(threadIdx.x % 32 == 0){/////%tid %ntid %laneid %warpid %nwarpid %ctaid %nctaid %smid %nsmid %gridid
-			printf("###1###warpid: %d, ctaid: %d, blockIdx.x: %d\n", warpid, ctaid, blockIdx.x );
+			printf("###1###warpid: %d, ctaid: %d, blockIdx.x: %d, nctaid: %d \n", warpid, ctaid, blockIdx.x, nctaid );
 		}			
 	}
 	
@@ -191,14 +193,14 @@ int main(int argc, char **argv)
 	///32 * 64 <==> 1 * 512 * 1024 (8gb), 32 * 512 <==> 1 * 64 * 1024 (8gb), 
 	///is it still true that in multi threads the dynamic page threshold is still 64k? no, it seems to be 2k.
 	//for(long long int data_stride = 1 * 1 * 1; data_stride <= 1 * 512 * 1024; data_stride = data_stride * 2){////not necessarily migrating whole 2m page. (not fair comparison but interesting to look at) 512 * 1024 is 4m, see what happens after 2m.
-	for(long long int data_stride = 1 * 1 * 256; data_stride <= 1 * 1 * 256; data_stride = data_stride * 2){///test
+	for(long long int data_stride = 1 * 1 * 128; data_stride <= 1 * 1 * 128; data_stride = data_stride * 2){///test
 	for(long long int mod = 536870912; mod <= 536870912; mod = mod * 2){////134217728 = 1gb, 268435456 = 2gb, 536870912 = 4gb, 1073741824 = 8gb, 2147483648 = 16gb, 4294967296 = 32gb, 8589934592 = 64gb. (index)
 	//for(long long int clock_count = 128; clock_count <= 8192; clock_count = clock_count * 2){/////11 positions.
 	for(long long int clock_count = 1; clock_count <= 1; clock_count = clock_count * 2){/////test
 		///////////////////////////////////////////////////////////////////CPU data begin		
 		//long long int data_size = mod;
 		long long int data_size = data_stride;
-		data_size = data_size * 8192;
+		data_size = data_size * 16384;
 		data_size = data_size * 512;
 		//long long int iterations = mod / data_stride;////32 * 32 * 4 / 32 * 2 = 256
 	
