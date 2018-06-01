78dd6b2c2fbe0bdbe9953a4eea6fe9bf91ca48a8
woodun <hwang07@email.wm.edu>
2018-06-01 13:33:57 -0700

    debug

--- runtime_large_data/runtime_large_data.cu
+++ runtime_large_data/runtime_large_data.cu
@@ -168,7 +168,7 @@ int main(int argc, char **argv)
 	printf("###################\n#########################managed\n");	
 	//////////////0.5gb to 64gb stride 0.5gb (1 * 4 * 1024)
 	//for(long long int data_stride = 1 * 1 * 2; data_stride <= 1 * 1 * 256; data_stride = data_stride + 1 * 1 * 2){/////512 is 4m, see what happens after 2m. 128 positions.
-	for(long long int data_stride = 1 * 1 * 256; data_stride <= 1 * 1 * 256; data_stride = data_stride + 1 * 1 * 2){////test
+	for(long long int data_stride = 1 * 1 * 128; data_stride <= 1 * 1 * 128; data_stride = data_stride + 1 * 1 * 2){////test
 	//for(long long int data_stride = 1 * 256 * 1024; data_stride <= 1 * 256 * 1024; data_stride = data_stride + 1 * 8 * 1024){
 	for(long long int mod = 4294967296; mod <= 4294967296; mod = mod * 2){////134217728 = 1gb, 268435456 = 2gb, 536870912 = 4gb, 1073741824 = 8gb, 2147483648 = 16gb, 4294967296 = 32gb, 8589934592 = 64gb. (index)
 	for(long long int clock_count = 128; clock_count <= 128; clock_count = clock_count * 2){
