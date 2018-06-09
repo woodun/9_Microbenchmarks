8580f5f2fbd4bb63ea66d798d610256908a0c726
woodun <hwang07@email.wm.edu>
2018-06-08 19:29:44 -0700

    debug

--- runtime_idea_test3/runtime_idea_test3.cu
+++ runtime_idea_test3/runtime_idea_test3.cu
@@ -157,14 +157,16 @@ int main(int argc, char **argv)
 	//when was 64k and 4k pages used?
 	//how to decrease the overhead of sync?
 	//printf("###################\n#########################managed\n");
-	/*
+	
 	for(long long int factor = 1; factor <= 128; factor = factor * 2){
 	for(double data_stride = 1 * 1 * 0.25 * factor; data_stride <= 1 * 1 * 4 * factor; data_stride = data_stride * 2){///134217728 = 1gb, 268435456 = 2gb, 536870912 = 4gb, 1073741824 = 8gb, 2147483648 = 16gb, 4294967296 = 32gb, 8589934592 = 64gb. (index)
 	for(long long int clock_count = 512; clock_count <= 16384; clock_count = clock_count * 2){
-	*/
-	for(long long int factor = 1; factor <= 4; factor = factor * 2){
-	for(double data_stride = 1 * 1 * 1 * factor; data_stride <= 1 * 1 * 4 * factor; data_stride = data_stride * 2){///134217728 = 1gb, 268435456 = 2gb, 536870912 = 4gb, 1073741824 = 8gb, 2147483648 = 16gb, 4294967296 = 32gb, 8589934592 = 64gb. (index)
+		
+	/*
+	for(long long int factor = 1; factor <= 1; factor = factor * 2){
+	for(double data_stride = 1 * 1 * 1 * factor; data_stride <= 1 * 1 * 1 * factor; data_stride = data_stride * 2){///134217728 = 1gb, 268435456 = 2gb, 536870912 = 4gb, 1073741824 = 8gb, 2147483648 = 16gb, 4294967296 = 32gb, 8589934592 = 64gb. (index)
 	for(long long int clock_count = 4096; clock_count <= 4096; clock_count = clock_count * 2){
+	*/
 		///////////////////////////////////////////////////////////////////CPU data begin		
 		long long int data_size = data_stride * 512;
 		data_size = data_size * 8192 * 512 / factor;
