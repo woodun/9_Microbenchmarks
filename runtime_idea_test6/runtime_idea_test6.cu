d005f25905f0542c7a2126166ad9591a84bb948d
woodun <hwang07@email.wm.edu>
2018-06-15 01:42:11 -0400

    debug

--- runtime_idea_test6/runtime_idea_test6.cu
+++ runtime_idea_test6/runtime_idea_test6.cu
@@ -239,12 +239,11 @@ int main(int argc, char **argv)
 	for(long long int clock_count = 64; clock_count <= 16384; clock_count = clock_count * 2){
 	*/
 
-	for(long long int offset = 8; offset <= 256; offset = offset * 2){
-		
 	printf("############approach\n");
-	for(long long int factor = 1; factor <= 1; factor = factor * 2){
-	for(double data_stride = 1 * 1 * 1 * factor; data_stride <= 1 * 1 * 1 * factor; data_stride = data_stride * 2){///134217728 = 1gb, 268435456 = 2gb, 536870912 = 4gb, 1073741824 = 8gb, 2147483648 = 16gb, 4294967296 = 32gb, 8589934592 = 64gb. (index)
-	for(long long int clock_count = 32; clock_count <= 4096; clock_count = clock_count * 2){
+	for(long long int offset = 64; offset <= 64; offset = offset * 2){	
+	for(long long int factor = 1; factor <= 128; factor = factor * 2){
+	for(double data_stride = 0.25 * factor; data_stride <= 1 * 1 * 4 * factor; data_stride = data_stride * 2){///134217728 = 1gb, 268435456 = 2gb, 536870912 = 4gb, 1073741824 = 8gb, 2147483648 = 16gb, 4294967296 = 32gb, 8589934592 = 64gb. (index)
+	for(long long int clock_count = 32; clock_count <= 32; clock_count = clock_count * 2){
 
 		///////////////////////////////////////////////////////////////////CPU data begin
 		double temp = data_stride * 512;
@@ -303,16 +302,15 @@ int main(int argc, char **argv)
 	}
 	printf("\n");
 	}
-	//printf("####################%llu\n", factor);
+	printf("####################%llu\n", factor);
+	}
 	printf("####################%llu\n", offset);
 	}
 	
-	}
-	
 	printf("############baseline\n");
-	for(long long int factor = 1; factor <= 1; factor = factor * 2){
-	for(double data_stride = 1 * 1 * 1 * factor; data_stride <= 1 * 1 * 1 * factor; data_stride = data_stride * 2){///134217728 = 1gb, 268435456 = 2gb, 536870912 = 4gb, 1073741824 = 8gb, 2147483648 = 16gb, 4294967296 = 32gb, 8589934592 = 64gb. (index)
-	for(long long int clock_count = 32; clock_count <= 4096; clock_count = clock_count * 2){
+	for(long long int factor = 1; factor <= 128; factor = factor * 2){
+	for(double data_stride = 0.25 * factor; data_stride <= 1 * 1 * 4 * factor; data_stride = data_stride * 2){///134217728 = 1gb, 268435456 = 2gb, 536870912 = 4gb, 1073741824 = 8gb, 2147483648 = 16gb, 4294967296 = 32gb, 8589934592 = 64gb. (index)
+	for(long long int clock_count = 32; clock_count <= 32; clock_count = clock_count * 2){
 
 		///////////////////////////////////////////////////////////////////CPU data begin
 		double temp = data_stride * 512;
