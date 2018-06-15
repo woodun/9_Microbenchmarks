192c553da4d8ace333285067b481710698d9674d
woodun <hwang07@email.wm.edu>
2018-06-14 18:44:27 -0700

    debug

--- runtime_idea_test6/runtime_idea_test6.cu
+++ runtime_idea_test6/runtime_idea_test6.cu
@@ -136,7 +136,7 @@ __global__ void page_visitor(long long int *A1, long long int *A2, long long int
 	B[index] = value1 + value2;	
 }
 
-__global__ void page_visitor3(long long int *A1, long long int *B1, double data_stride, long long int clock_count, long long int offset){////vertical + horizontal
+__global__ void page_visitor3(long long int *A1, long long int *A2, long long int *B1, double data_stride, long long int clock_count, long long int offset){////vertical + horizontal
 
 	double temp = (blockIdx.x * 512 + threadIdx.x) * data_stride;
 	long long int index = __double2ll_rd(temp);
@@ -308,10 +308,12 @@ int main(int argc, char **argv)
 	printf("####################%llu\n", offset);
 	}
 	
+	}
+	
 	printf("############baseline\n");
 	for(long long int factor = 1; factor <= 1; factor = factor * 2){
 	for(double data_stride = 1 * 1 * 1 * factor; data_stride <= 1 * 1 * 1 * factor; data_stride = data_stride * 2){///134217728 = 1gb, 268435456 = 2gb, 536870912 = 4gb, 1073741824 = 8gb, 2147483648 = 16gb, 4294967296 = 32gb, 8589934592 = 64gb. (index)
-	for(long long int clock_count = 8; clock_count <= 4096; clock_count = clock_count * 2){
+	for(long long int clock_count = 32; clock_count <= 4096; clock_count = clock_count * 2){
 
 		///////////////////////////////////////////////////////////////////CPU data begin
 		double temp = data_stride * 512;
