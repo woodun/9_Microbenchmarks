74b362263ec6f92101afd2ef1ddf51e5fcb0fcf4
woodun <hwang07@email.wm.edu>
2018-06-19 19:02:29 -0700

    debug

--- runtime_idea_test4/runtime_idea_test4.cu
+++ runtime_idea_test4/runtime_idea_test4.cu
@@ -346,7 +346,7 @@ int main(int argc, char **argv)
 	for(long long int clock_count = 64; clock_count <= 16384; clock_count = clock_count * 2){
 	*/
 
-	/*
+	///*
 	printf("############approach\n");
 	for(long long int time = 32; time <= 32; time = time * 2){
 	printf("####################time: %llu\n", time);
@@ -416,7 +416,7 @@ int main(int argc, char **argv)
 
 		////may want to use more thread to see clock_count effect
 		//page_visitor5<<<8192 * 512 / factor, 512 + 32>>>(CPU_data_in1, GPU_data_out1, data_stride, clock_count, offset);
-		page_visitor3<<<8192 * 512 / factor, 512 + 32>>>(CPU_data_in1, GPU_data_out1, data_stride, clock_count, offset);
+		page_visitor3<<<8192 * 512 / factor, 512>>>(CPU_data_in1, GPU_data_out1, data_stride, clock_count, offset);
 		cudaDeviceSynchronize();
 				
 		/////////////////////////////////time
@@ -438,9 +438,9 @@ int main(int argc, char **argv)
 	}
 	}
 	}
-	*/
+	//*/
 	
-	///*
+	/*
 	printf("############baseline\n");
 	for(long long int factor = 16384; factor <= 16384; factor = factor * 2){
 	for(double data_stride = 1 * 1 * 1 * factor; data_stride <= 1 * 1 * 1 * factor; data_stride = data_stride * 2){///134217728 = 1gb, 268435456 = 2gb, 536870912 = 4gb, 1073741824 = 8gb, 2147483648 = 16gb, 4294967296 = 32gb, 8589934592 = 64gb. (index) 262144 = 2m. 16384 = 128k.
@@ -498,7 +498,7 @@ int main(int argc, char **argv)
 	}
 	//printf("####################%llu\n", factor);
 	}
-	//*/
+	*/
 	
 	exit(EXIT_SUCCESS);
 }
