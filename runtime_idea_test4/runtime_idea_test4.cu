e083df3b8545846c3565d1d549d5f59773db547c
woodun <hwang07@email.wm.edu>
2018-06-20 14:45:11 -0700

    debug

--- runtime_idea_test4/runtime_idea_test4.cu
+++ runtime_idea_test4/runtime_idea_test4.cu
@@ -369,12 +369,14 @@ int main(int argc, char **argv)
 		//if(offset2 == 2){
 		//	offset = 1;
 		//}
-	printf("############offset: %llu\n", offset);
+	//printf("############offset: %llu\n", offset);
 	
 	for(long long int factor = 16384; factor <= 16384; factor = factor * 2){
-	printf("####################factor: %llu\n", factor);	
+	//printf("####################factor: %llu\n", factor);
 	
 	for(double data_stride = 1 * 1 * 1 * factor; data_stride <= 1 * 1 * 1 * factor; data_stride = data_stride * 2){///134217728 = 1gb, 268435456 = 2gb, 536870912 = 4gb, 1073741824 = 8gb, 2147483648 = 16gb, 4294967296 = 32gb, 8589934592 = 64gb. (index)
+	//printf("\n");
+
 	for(long long int clock_count = 16384; clock_count <= 65536; clock_count = clock_count * 2){
 		
 	///long long int time2 = time;
@@ -430,8 +432,7 @@ int main(int argc, char **argv)
 		
 		checkCudaErrors(cudaFree(CPU_data_in1));		
 		checkCudaErrors(cudaFree(GPU_data_out1));
-	}
-	printf("\n");
+	}	
 	}
 	}
 	}
@@ -441,9 +442,13 @@ int main(int argc, char **argv)
 	//*/
 	
 	///*
-	printf("############baseline\n");
+	printf("\n############baseline\n");
 	for(long long int factor = 16384; factor <= 16384; factor = factor * 2){
+	//printf("####################factor: %llu\n", factor);
+		
 	for(double data_stride = 1 * 1 * 1 * factor; data_stride <= 1 * 1 * 1 * factor; data_stride = data_stride * 2){///134217728 = 1gb, 268435456 = 2gb, 536870912 = 4gb, 1073741824 = 8gb, 2147483648 = 16gb, 4294967296 = 32gb, 8589934592 = 64gb. (index) 262144 = 2m. 16384 = 128k.
+	//printf("\n");
+	
 	for(long long int clock_count = 16384; clock_count <= 65536; clock_count = clock_count * 2){
 
 		///////////////////////////////////////////////////////////////////CPU data begin
@@ -493,10 +498,8 @@ int main(int argc, char **argv)
 		
 		checkCudaErrors(cudaFree(CPU_data_in1));		
 		checkCudaErrors(cudaFree(GPU_data_out1));
-	}
-	printf("\n");
-	}
-	//printf("####################%llu\n", factor);
+	}	
+	}	
 	}
 	//*/
 	
