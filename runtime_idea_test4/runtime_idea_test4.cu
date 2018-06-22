a140ed360babb63ee25cffbe7d5de3cb8e06e4bb
woodun <hwang07@email.wm.edu>
2018-06-21 18:27:58 -0700

    debug

--- runtime_idea_test4/runtime_idea_test4.cu
+++ runtime_idea_test4/runtime_idea_test4.cu
@@ -176,7 +176,7 @@ __global__ void page_visitor3(long long int *A1, long long int *B1, double data_
 	
 	if(threadIdx.x < 32){			
 		if(blockIdx.x < 4194304 - offset){
-			if(blockIdx.x % 4 == 0){
+			if(blockIdx.x % 8 == 0){
 				B1[prefetch_index] = 0;
 			}
 		}
