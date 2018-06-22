bdbd3a66fc8f46f37981e7d886b90b2c57d6fdad
woodun <hwang07@email.wm.edu>
2018-06-22 03:24:23 -0400

    debug

--- runtime_idea_test4/runtime_idea_test4.cu
+++ runtime_idea_test4/runtime_idea_test4.cu
@@ -502,7 +502,7 @@ int main(int argc, char **argv)
 		//}
 	//printf("############offset: %llu\n", offset);
 	
-	for(long long int factor = 8; factor <= 8; factor = factor * 2){/////////////16384 (128k) max
+	for(long long int factor = 16384; factor <= 16384; factor = factor * 2){/////////////16384 (128k) max
 	//printf("####################factor: %llu\n", factor);
 	
 	for(double data_stride = 1 * 1 * 1 * factor; data_stride <= 1 * 1 * 1 * factor; data_stride = data_stride * 2){///134217728 = 1gb, 268435456 = 2gb, 536870912 = 4gb, 1073741824 = 8gb, 2147483648 = 16gb, 4294967296 = 32gb, 8589934592 = 64gb. (index)
@@ -576,7 +576,7 @@ int main(int argc, char **argv)
 	
 	///*
 	printf("\n############baseline\n");
-	for(long long int factor = 8; factor <= 8; factor = factor * 2){/////////////16384 max
+	for(long long int factor = 16384; factor <= 16384; factor = factor * 2){/////////////16384 max
 	//printf("####################factor: %llu\n", factor);
 		
 	for(double data_stride = 1 * 1 * 1 * factor; data_stride <= 1 * 1 * 1 * factor; data_stride = data_stride * 2){///134217728 = 1gb, 268435456 = 2gb, 536870912 = 4gb, 1073741824 = 8gb, 2147483648 = 16gb, 4294967296 = 32gb, 8589934592 = 64gb. (index) 262144 = 2m. 16384 = 128k.
