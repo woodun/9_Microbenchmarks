ac48e9aa26a2918b3f3f1ced8804a1c0c3a98ef7
woodun <hwang07@email.wm.edu>
2018-06-26 15:34:08 -0700

    debug

--- fault_group_test/fault_group_test.cu
+++ fault_group_test/fault_group_test.cu
@@ -75,8 +75,10 @@ __global__ void page_visitor(long long int *A1, long long int *B1, double data_s
 	//double temp = (threadIdx.x % 32) * 1;
 	//double temp = (threadIdx.x) * 1;
 	//double temp = (threadIdx.x % 32) * 2 + warpid * 1;
-	double temp = (threadIdx.x) * 512;
+	//double temp = (threadIdx.x) * 512;
 	//double temp = (threadIdx.x % 32) * 1024 + warpid * 512;
+	//double temp = (threadIdx.x) * 512;
+	double temp = (threadIdx.x % 32) * 2048 + warpid * 512;
 	long long int index = __double2ll_rd(temp);
 	long long int value1;
 
@@ -209,7 +211,7 @@ int main(int argc, char **argv)
 
 		////may want to use more thread to see clock_count effect		
 		//page_visitor<<<8192 * 512 / factor, 512>>>(CPU_data_in1, GPU_data_out1, data_stride, clock_count);
-		page_visitor<<<1, 64>>>(CPU_data_in1, GPU_data_out1, data_stride, clock_count);		
+		page_visitor<<<1, 128>>>(CPU_data_in1, GPU_data_out1, data_stride, clock_count);		
 		cudaDeviceSynchronize();
 				
 		/////////////////////////////////time
