e8f347869f23838852403deac7459df401e7b196
woodun <hwang07@email.wm.edu>
2018-06-25 19:30:08 -0700

    debug

--- fault_group_test/fault_group_test.cu
+++ fault_group_test/fault_group_test.cu
@@ -71,8 +71,8 @@ __global__ void page_visitor(long long int *A1, long long int *B1, double data_s
     asm("mov.u32 %0, %warpid;" : "=r"(warpid));
     
 	//double temp = (blockIdx.x * blockDim.x + threadIdx.x) * 8388608;
-	double temp = warpid * 1 * 16 + (threadIdx.x % 16) * 1;
-	//double temp = (threadIdx.x % 32) * 1;
+	//double temp = warpid * 1 * 16 + (threadIdx.x % 16) * 1;
+	double temp = (threadIdx.x % 32) * 1;
 	long long int index = __double2ll_rd(temp);
 	long long int value1;
 
