de236aaa3667beb9486d62aaaa8d4b5449d557b0
woodun <hwang07@email.wm.edu>
2018-06-26 18:43:04 -0700

    debug

--- fault_group_test/fault_group_test.cu
+++ fault_group_test/fault_group_test.cu
@@ -85,8 +85,8 @@ __global__ void page_visitor(long long int *A1, long long int *B1, double data_s
 	//double temp = (threadIdx.x % 32) * 16 + warpid * 1;
 	//double temp = (blockIdx.x * blockDim.x + threadIdx.x) * 1;
 	//double temp = ((blockIdx.x * blockDim.x + threadIdx.x) % 32) * 2 + warpid * 1;
-	double temp = (blockIdx.x * blockDim.x + threadIdx.x) * 1;
-	//double temp = ((blockIdx.x * blockDim.x + threadIdx.x) % 32) * 2 + warpid * 1;
+	//double temp = (blockIdx.x * blockDim.x + threadIdx.x) * 1;
+	double temp = ((blockIdx.x * blockDim.x + threadIdx.x) % 32) * 2 + warpid * 1;
 	long long int index = __double2ll_rd(temp);
 	long long int value1;
 
