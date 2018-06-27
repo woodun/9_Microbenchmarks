23b6c01445df52cb818e3d8107ca662545627d3c
woodun <hwang07@email.wm.edu>
2018-06-27 14:33:34 -0700

    debug

--- fault_group_test/fault_group_test.cu
+++ fault_group_test/fault_group_test.cu
@@ -195,7 +195,8 @@ __global__ void page_visitor3(long long int *A1, long long int *B1, double data_
 		while (clock_offset < 1024){/////////////////what's the time overhead for addition and multiplication?
 			clock_offset++;
 			//value1 = value1 * 3;
-			asm("mul.lo.s64 %0, %1, 7;" : "=l"(value1) : "l"(value1));			
+			asm("mul.lo.s64 %0, %1, 7;" : "=l"(value1) : "l"(value1));
+			asm("div.s64 %0, %1, 3;" : "=l"(value1) : "l"(value1));				
 		}
 	}
 	
