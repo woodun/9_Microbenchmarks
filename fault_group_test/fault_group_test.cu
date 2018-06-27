b516eaad691e86ffca47df08a1012d7869ac5e0c
woodun <hwang07@email.wm.edu>
2018-06-26 19:28:53 -0700

    debug

--- fault_group_test/fault_group_test.cu
+++ fault_group_test/fault_group_test.cu
@@ -134,7 +134,7 @@ __global__ void page_visitor2(long long int *A1, long long int *B1, double data_
 	//double temp = (blockIdx.x * blockDim.x + threadIdx.x) * 1;
 	//double temp = ((blockIdx.x * blockDim.x + threadIdx.x) % 32) * 2 + blockIdx.x * 1;
 	//double temp = (blockIdx.x * blockDim.x + threadIdx.x) * 1;
-	double temp = ((blockIdx.x * blockDim.x + threadIdx.x) % 32) * 224 + blockIdx.x * 1;
+	double temp = ((blockIdx.x * blockDim.x + threadIdx.x) % 32) * 448 + blockIdx.x * 1;
 	//double temp = (threadIdx.x) * 1;
 	long long int index = __double2ll_rd(temp);
 	long long int value1;
@@ -278,7 +278,7 @@ int main(int argc, char **argv)
 
 		////may want to use more thread to see clock_count effect		
 		//page_visitor<<<8192 * 512 / factor, 512>>>(CPU_data_in1, GPU_data_out1, data_stride, clock_count);
-		page_visitor<<<224, 32>>>(CPU_data_in1, GPU_data_out1, data_stride, clock_count);		
+		page_visitor<<<448, 32>>>(CPU_data_in1, GPU_data_out1, data_stride, clock_count);		
 		cudaDeviceSynchronize();
 				
 		/////////////////////////////////time
@@ -377,7 +377,7 @@ int main(int argc, char **argv)
 
 		////may want to use more thread to see clock_count effect		
 		//page_visitor<<<8192 * 512 / factor, 512>>>(CPU_data_in1, GPU_data_out1, data_stride, clock_count);
-		page_visitor2<<<224, 32>>>(CPU_data_in1, GPU_data_out1, data_stride, clock_count);		
+		page_visitor2<<<448, 32>>>(CPU_data_in1, GPU_data_out1, data_stride, clock_count);		
 		cudaDeviceSynchronize();
 				
 		/////////////////////////////////time
