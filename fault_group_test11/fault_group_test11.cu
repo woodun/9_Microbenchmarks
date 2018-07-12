e8d53f2263872e4ead8dd2ab4492361b638a5261
woodun <hwang07@email.wm.edu>
2018-07-11 18:50:59 -0700

    debug

--- fault_group_test11/fault_group_test11.cu
+++ fault_group_test11/fault_group_test11.cu
@@ -151,8 +151,8 @@ int main(int argc, char **argv)
 	
 	
 
-	long long int num_thread = 512;
-	long long int size_of_data = 1048576;
+	long long int num_thread = 256;
+	long long int size_of_data = 524288;
 	///*
 	//printf("############approach\n");
 	for(long long int time = 0; time <= 0; time = time + 1){
