0aadc290d4ac4599a8b8cf157a0ec7fbba3c03fd
woodun <hwang07@email.wm.edu>
2018-07-13 12:13:50 -0700

    debug

--- runtime_idea_test8/runtime_idea_test8.cu
+++ runtime_idea_test8/runtime_idea_test8.cu
@@ -499,7 +499,8 @@ int main(int argc, char **argv)
 	*/
 
 	printf("############approach\n");
-	for(long long int time = 1; time <= 128; time = time * 2){
+	//for(long long int time = 1; time <= 128; time = time * 2){
+	for(long long int time = 0; time <= 128; time = time + 32){
 	printf("\n####################time: %llu\n", time);
 	
 	long long int coverage2 = 0;
@@ -529,7 +530,7 @@ int main(int argc, char **argv)
 	for(double data_stride = 1 * 1 * 1 * factor; data_stride <= 1 * 1 * 1 * factor; data_stride = data_stride * 2){///134217728 = 1gb, 268435456 = 2gb, 536870912 = 4gb, 1073741824 = 8gb, 2147483648 = 16gb, 4294967296 = 32gb, 8589934592 = 64gb. (index)
 	//printf("\n");
 
-	for(long long int clock_count = 1; clock_count <= 128; clock_count = clock_count * 2){
+	for(long long int clock_count = 128; clock_count <= 128; clock_count = clock_count * 2){
 		
 	long long int time2 = time;
 	if(time2 > clock_count){
