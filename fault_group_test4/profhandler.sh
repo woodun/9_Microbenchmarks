cc795a00115bcc5e6915bfb615c62be28443e93b
woodun <hwang07@email.wm.edu>
2018-07-20 18:47:59 -0700

    debug

---
+++ fault_group_test4/profhandler.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+
+grep 'Unified Memory GPU page faults' $1 | sed 's/.*"\([0-9][0-9]*\)",.*,"\[Unified Memory GPU page faults\]"/\1/' > temp_faults.txt
+grep 'Unified Memory Memcpy HtoD' $1 > temp_migrations.txt
+
+#number of fault groups
+cat temp_faults.txt | wc -l
+
+echo " "
+
+#number of faults
+cat temp_faults.txt | awk '{ SUM += $1} END { print SUM }'
+
+echo " "
+
+#number of page migration
+cat temp_migrations.txt | wc -l
+
+
+rm temp_faults.txt
+rm temp_migrations.txt
+
