faae7433b887cc48c9fea5a8243773741c40ba93
woodun <hwang07@email.wm.edu>
2018-07-25 14:35:09 -0700

    debug

--- fault_group_test4/launch_all.sh
+++ fault_group_test4/launch_all.sh
@@ -2,6 +2,12 @@
 
 for i in 256 512 1024 2048 4096 8192 16384 32768 65536 131072 262144 524288
 do
-nvprof --profile-from-start off --print-gpu-trace --log-file prof832size8gpage$i.txt --csv ./fault_group_test4 $i
+./fault_group_test4 $i
 done
+
+#for i in 256 512 1024 2048 4096 8192 16384 32768 65536 131072 262144 524288
+#do
+#nvprof --profile-from-start off --print-gpu-trace --log-file prof832size8gpage$i.txt --csv ./fault_group_test4 $i
+#done
+
 
