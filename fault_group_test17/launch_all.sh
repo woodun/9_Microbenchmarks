c5fc8c58742cd6baa4f57a9c14996335ef0be9b8
woodun <hwang07@email.wm.edu>
2018-07-27 05:41:15 -0400

    debug

--- fault_group_test17/launch_all.sh
+++ fault_group_test17/launch_all.sh
@@ -9,7 +9,7 @@ echo " "
 
 for i in 256 512 1024 2048 4096 8192 16384 32768 65536 131072 262144 524288 1048576	2097152 4194304 8388608 16777216 33554432 67108864 134217728
 do
-nvprof --profile-from-start off --print-gpu-trace --log-file prof132size8gpage$i.txt --csv ./fault_group_test17 $i
+nvprof --profile-from-start off --print-gpu-trace --log-file prof164size8gpage$i.txt --csv ./fault_group_test17 $i
 done
 
 echo " "
